ecb3c17471e715ba4c4cd36e9d34a5724c45475e
Chavi Awasthy <93931120+chaviawasthy@users.noreply.github.com>
2024-01-26 23:52:36 +0530

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -51,33 +51,4 @@ export const projects = [
     image: meme,
     github: 'https://github.com/chaviawasthy/meme-generator',
   },
-  // {
-  //   title: 'Smart Attendance App',
-  //   description:
-  //     'A mobile app in which the user can track and mark attendance using face verification.',
-  //   image:
-  //     'https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/smartattendance.png',
-  //   github: 'https://github.com/teshank2137/smartAtendence',
-  // },
-  // {
-  //   title: 'Stock Market Prediction API',
-  //   description:
-  //     "An API Which predicts the next day's closing price of a stock.",
-  //   image: stonks,
-  //   github: 'https://github.com/teshank2137/stockprediction',
-  // },
-  // {
-  //   title: "Algorithm Visualizer",
-  //   description: "Web App to visualize the searching and sorting algorithms",
-  //   image:
-  //     "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/sort.JPG",
-  //   link: "https://teshank2137.github.io/#/",
-  // },
-  // {
-  //   title: "Portfolio v1",
-  //   description: "Built using ReactJS, ThreeJS, CSS3 and lots of ❤️ love.",
-  //   image:
-  //     "https://raw.githubusercontent.com/teshank2137/portfolio/v1/src/assets/images/port.JPG",
-  //   link: "https://www.v1.teshank.dev/",
-  // },
 ];
